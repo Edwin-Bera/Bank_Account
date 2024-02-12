380b46f9fccb9553fd4bbf7a55a1e6c527e2b25d
Edwin-Bera <blacksenyin@gmail.com>
2024-02-12 23:20:22 +0200

    Add Date Functionality

--- Headers/Bank.h
+++ Headers/Bank.h
@@ -5,21 +5,29 @@ using namespace std;
 class BankAccount
 {
 	protected:
-	string name, number;
+	string name; //name of the account holder
+	string number; //account number
+	int date[3]; //date of account creation. To be used when calculating interest, maturity and service charges
 	double balance;
 
 	public:
 
-	BankAccount(string owner, string accNum, double bal);
+	BankAccount(string owner, string accNum, string dt, double bal);
 	string retrieve_Name();
 	string retrieve_Num();
+	string retrieve_Date();
 	double retrieve_Bal();
 	void set_Name(string owner);
 	void set_Num(string accNum);
 	void set_Bal(double bal);
+	void set_Date(string dt);
 	virtual void monthly_statement() = 0;
 	virtual void withdraw(double amount) = 0;
 	void deposit(double amount);
 	bool checkNum(string accNum);
+
+	private:
+
+	string invert(string input);
 };
 #endif
--- Implementations/Bank.cpp
+++ Implementations/Bank.cpp
@@ -1,5 +1,6 @@
 #include "../Headers/Bank.h"
 #include <string>
+#include <cmath>
 using namespace std;
 bool BankAccount::checkNum(string accNum)
 {
@@ -25,6 +26,47 @@ void BankAccount::set_Name(string owner)
 		name = owner;
 }
 
+void BankAccount::set_Date(string dt)
+{
+
+	for (int index = 2, cursor = dt.length() - 1; cursor >= 0; index--, cursor--)
+	{
+		for (int power = 0; dt[cursor] != '/' && cursor >= 0; cursor--, power++)
+		{
+			date[index] += (static_cast<int>(dt[cursor]) - 48) * pow(10, power);
+			//cout << (static_cast<int>(dt[cursor]) - 48) * pow(10, power)<<  endl;
+		}
+	}
+}
+
+string BankAccount::retrieve_Date()
+{
+	string output = "";
+	int d[3];
+	int remainder;
+	for (int i = 0; i < 3; i++)
+		d[i] = date[i];
+	for (int index = 0; index < 3; index++)
+	{
+		for (int power = 1; d[index]; power++)
+		{
+			remainder = d[index] % static_cast<int>(pow(10, power));
+			d[index] -= remainder;
+			remainder /= pow(10, power - 1);
+			output += static_cast<char>(remainder + 48);
+		}
+		if (index < 2)
+			output += '/';
+	}
+	return invert(output);
+}
+string BankAccount::invert(string input)
+{
+	string output;
+	for (int index = input.length() - 1; index >= 0; index--)
+		output+= input[index];
+	return output;
+}
 void BankAccount::set_Bal(double bal)
 {
 	if (number[0] == 'I')
@@ -34,11 +76,12 @@ void BankAccount::set_Bal(double bal)
 }
 
 
-BankAccount::BankAccount(string owner, string accNum, double bal)
+BankAccount::BankAccount(string owner, string accNum,string dt, double bal)
 {
 
 	set_Name(owner);
 	set_Num(accNum);
+	set_Date(dt);
 	set_Bal(bal);
 }
 
