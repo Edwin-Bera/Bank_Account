934cba8dbc5899456bec70a872c5911b8b1c130a
Edwin-Bera <blacksenyin@gmail.com>
2024-03-13 22:33:57 +0200

    ADD derived class 'noServiceCharge'

---
+++ Headers/noServiceChargeChecking.h
@@ -0,0 +1,14 @@
+#ifndef NSCC_H
+#define NSCC_H
+#include "check.h"
+class noServiceChargeChecking: public CheckingAccount
+{
+	const double annualInterest = 2.29, minimun_bal = 350;
+	public:
+  	noServiceChargeChecking(string owner, string num, string dt, double bal);
+ 	double write_check(double amount, string today, string recipient);
+	double calculateInterest(string today);
+	double checkBalance(string today);
+	void updateBalance(string today);
+};
+#endif
---
+++ Implementations/noServiceChargeChecking.cpp
@@ -0,0 +1,44 @@
+#include "../Headers/noServiceChargeChecking.h"
+#include <iostream>
+#include <string>
+using namespace std;
+noServiceChargeChecking::noServiceChargeChecking(string owner, string num, string dt, double bal):CheckingAccount(owner, num, dt, bal)
+{
+}
+
+double noServiceChargeChecking::calculateInterest(string today)
+{
+	return calculateDays(today) / 365 * annualInterest;
+}
+
+void noServiceChargeChecking::updateBalance(string today)
+{
+	double interest = calculateInterest(today);
+        if (interest)
+                balance += interest;
+}
+double noServiceChargeChecking::checkBalance(string today)
+{
+	updateBalance(today);
+	return balance;
+}
+
+ double noServiceChargeChecking::write_check(double amount, string today, string recipient)
+{
+
+        updateBalance(today);
+        if (!compareDates(today) || balance < amount)
+        {
+                cout << "Invalid transaction" << endl;
+                return 0.0;
+        }
+        balance -= amount;
+
+        cout << "Account Holder: " << name << endl;
+        cout << "Recipient:      " << recipient << endl;
+        cout << "Amount:         " << amount << endl;
+        cout << "Date:           " << today << endl;
+        return amount;
+
+}
+
