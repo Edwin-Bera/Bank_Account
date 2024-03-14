0100c20faa56620673e2d26c80814627b7d94ff5
Edwin-Bera <blacksenyin@gmail.com>
2024-03-14 14:11:10 +0200

    ADD derived class 'highInterestChecking'

---
+++ Implementations/highInterestChecking.cpp
@@ -0,0 +1,54 @@
+#include "../Headers/highInterestChecking.h"
+#include <string>
+#include <iostream>
+using namespace std;
+
+ highInterestChecking::highInterestChecking(string owner, string num, string dt, double bal):noServiceChargeChecking(owner, num, dt, bal)
+{
+	if (bal < min_bal)
+		set_Bal(0);
+	else
+	{
+		set_Bal(bal);
+		set_Name(owner);
+        	set_Num(num);
+        	set_Date(dt);
+	}
+      
+}
+double highInterestChecking::calculateInterest(string today)
+{
+        return calculateDays(today) / 365 * higherInterest;
+}
+
+/*void highInterestChecking::updateBalance(string today)
+{
+        double interest = calculateInterest(today);
+        if (interest)
+                balance += interest;
+}
+double highInterestChecking::checkBalance(string today)
+{
+        updateBalance(today);
+        return balance;
+}
+
+ double highInterestChargeChecking::write_check(double amount, string today, string recipient)
+{
+
+        updateBalance(today);
+        if (!compareDates(today) || balance < amount)
+        {
+                cout << "Invalid transaction" << endl;
+                return 0.0;
+        }
+        balance -= amount;
+
+        cout << "Account Holder: " << name << endl;
+        cout << "Recipient:      " << recipient << endl;
+        cout << "Amount:         " << amount << endl;
+        cout << "Date:           " << today << endl;
+        return amount;
+
+}*/
+
